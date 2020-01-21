aa04cdb4d0430c25a77422c6b8132ed800162452
Taishawn King <taishawnking1@yahoo.com>
2020-01-21 03:02:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -26,19 +26,8 @@ end
   #binding.pry
 end
 
-# require"pry"
-# class Artist
-#   attr_accessor :name, :song
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end 
-# def songs
-# Song.all.select {|song| song.artist == self }
-# end
-# def self.all
-#   @@all
-# end
+
+
 # def add_song(song)
 #   @songs << song
 #   song.artist = self
