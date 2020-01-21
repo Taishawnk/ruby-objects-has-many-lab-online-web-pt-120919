8db85c5ad9096f963fb53b0603c2ed38d8974c4c
Taishawn King <taishawnking1@yahoo.com>
2020-01-21 03:01:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,20 +5,16 @@ class Artist
     @name = name
     @songs = []
   end 
-  
 def songs
 Song.all.select {|song| song.artist == self }
 end
-
 def self.all
   @@all
 end
-  
 def add_song(song)
   @songs << song
   song.artist = self
 end
-  
 def add_song_by_name(song)
   song = Song.new(song)
   add_song(song)
