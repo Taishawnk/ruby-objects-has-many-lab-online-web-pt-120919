36eb004d020d818c88edc85ee4e0a2a8003d4cf2
Taishawn King <taishawnking1@yahoo.com>
2020-01-21 03:01:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,20 +5,16 @@ def initialize(name)
     @name = name
     @post = []
 end
-  
 def posts
   Author.all.select {|post| post.author == self }
 end
-  
 def self.all
   @@all
 end
-  
   def add_post(post)
     @posts << post
     post.author = self
   end
-  
   def add_post_by_title(post)
     post = Post.new(post)
     add_post(post)
