e9572357a87ce7bfbbbdaa362924c87ef2709c18
Taishawn King <taishawnking1@yahoo.com>
2020-01-21 03:03:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -23,23 +23,10 @@ end
   def self.post_count
    Author.all.length
   end
-  #binding.pry
+ 
 end
 
 
-
-# def add_song(song)
-#   @songs << song
-#   song.artist = self
-# end
-# def add_song_by_name(song)
-#   song = Song.new(song)
-#   add_song(song)
-# end
-# def self.song_count
-#   Song.all.length
-#   end
-# end
   
 
 
